0679556b64fd86ae1e11b0fe13ddcf0aea1f91d5
0xFarted <104228340+0xFadedd@users.noreply.github.com>
2023-05-13 07:36:31 +0100

    working on triangle checker

---
+++ C++/triangleChecker.cpp
@@ -0,0 +1,19 @@
+/*
+    A program that takes 3 angles a inputs and checks whether they form a valid triagle
+*/
+
+#include<iostream>
+
+using namespace std;
+
+// create check functions for each type of triangle
+
+int main() {
+    // print enter angle messages
+    // use cin to have things entered
+    //check they form a valid triangle and return the trianle type
+    return 0;
+}
+
+
+
