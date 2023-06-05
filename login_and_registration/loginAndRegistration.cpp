41a55ecb00f907806247d058e630275fd8dd4c3e
0xFarted <104228340+0xFadedd@users.noreply.github.com>
2023-06-05 23:36:32 +0100

    [C++ - L&R] finished sign up function

--- login_and_registration/loginAndRegistration.cpp
+++ login_and_registration/loginAndRegistration.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include <math.h>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
 class User {
     public:
-        string username;
-        string password;
-};
+        std::string username;
+    private:
+        std::string passwordHash;
 
+    public:
+        void setPasswordHash(string hash) {
+            passwordHash = hash;
+        }
+
+        string getPasswordHash() {
+            return passwordHash;
+        }
+};
 
 void signIn() {
     User user;
@@ -17,12 +27,29 @@ void signIn() {
     std::cout << "Please enter your username";
     std::cin >> user.username;
     std::cout << "Please enter your username";
-    std::cin >> user.password;
-    user.password = hash(user.password);
+    std::string p;
+    user.setPasswordHash((std::cin >> p, std::to_string(hash(p))));
 
+    std::string filename = user.username + ".txt";
+    std::ifstream file(filename);
+
+    if (!file) {
+        std::cerr << "Incorrect Username: " << filename;
+        return;
+    }
+
+    std::string fileLine;
+    while(getline(file, fileLine)) {
+        if(user.getPasswordHash() == fileLine) {
+            std::cout << "Success! You are now signed in.";
+        } else {
+            std::cout << "Incorrect Password, try again";
+        }   
+    }
 }
 
 void signUp() {
+    
 
 }
 
